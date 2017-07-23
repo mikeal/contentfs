3289579c09d4d85b1eec5d88d6e0d5df0f4c8945
Mikeal Rogers <mikeal.rogers@gmail.com>
2017-07-23 11:01:39 -0700

    fix(coverage): Finally ignoring if else statement.

--- index.js
+++ index.js
@@ -98,8 +98,7 @@ class ContentFS {
         _dir = _dir[p]
       // Once we hit this point it is never false so we need to ignore
       // coverage of the falsey branch of the if.
-      /* istanbul ignore if */
-      } else if (typeof value === 'undefined') {
+      } /* istanbul ignore next */ else if (typeof value === 'undefined') {
         _dir[p] = {}
         _dir = _dir[p]
       } else {
